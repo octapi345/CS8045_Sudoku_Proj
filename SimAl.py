40d7af85875e538baa00e05053930e18b8e99eae
Joseph Natter <josephnatter@MacBook-Pro.local>
2025-11-08 14:49:26 -0500

    made it abort after 5,000,000 iters

--- SimAl.py
+++ SimAl.py
@@ -179,7 +179,7 @@ class SimulatedAnnealing:
 
         return False
 
-    def solve(self, display=False, max_iters=10**7):
+    def solve(self, display=False, max_iters=5*(10**6)):
         min_T = 0.01
 
         while self.error_count > 0 and self.iters < max_iters:
